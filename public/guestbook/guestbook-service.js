3d4c83749021e45381cceb420952b6e70d98d07c
Alex Tomovski <alext@wix.com>
2015-04-20 14:47:36 +0300

    Refactor based on comments and elementory 2.0 - Removed the webmodule as it is un-needed with the new authorization model. - Scripts src are now imports - The guestbook-service is the one responsible for working with Wix-Data. - On add we dont include a date anymore as it is totally useless. We just use the createdDate property.

--- public/guestbook/guestbook-service.js
+++ public/guestbook/guestbook-service.js
@@ -1,23 +1,64 @@
-//TODO move this into the folders once we have the import bug fixed.
-import * as guestBookStorage from 'backend/guestbookstorage'
-import wixData from 'wix-data'
+import wixData from 'wix-data';
 
 angular.module('guestbook')
 	.factory('guestBookService',['$q', function ($q) {
 		'use strict';
+		
+		function saveEntry(entry) {
+			return wixData.save('entries', entry)
+				.then(function success(data) {
+					return data;
+				});
+		}
+		
+		function removeEntry(entry) {
+			return wixData.remove('entries', entry._id)
+				.then(function success(data) {
+					return data;
+				});
+		}
+		
+		function loadEntries() {
+			return wixData.query('entries')
+				.descending('_createdDate')
+				.limit(3)
+				.find()
+				.then(function success(data){
+					return data;
+				});
+		}
+		
+		function loadPage(pageNumber) {
+			if(pageNumber === 1){
+				return loadEntries();
+			}
+			
+			return wixData.query('entries')
+				.descending('_createdDate')
+				.skip(pageNumberToSkip(pageNumber))
+				.limit(3)
+				.find()
+				.then(function success(data) {
+					return data;
+				});
+		}
+		
+		function pageNumberToSkip(pageNumber) {
+			return 3 * (pageNumber-1);
+		}
 
 		return {
 			saveEntry: function(entry) {				
-				return $q.when(guestBookStorage.saveEntry(entry));
+				return $q.when(saveEntry(entry));
 			},
 			removeEntry: function(entry) {
-				return $q.when(guestBookStorage.removeEntry(entry));	
+				return $q.when(removeEntry(entry));	
 			},
 			loadEntries: function() {
-				return $q.when(guestBookStorage.loadEntries());	
+				return $q.when(loadEntries());	
 			},
 			loadPage: function(pageId) {
-				return $q.when(guestBookStorage.loadPage(pageId));
+				return $q.when(loadPage(pageId));
 			}
 		};
 	}]);
