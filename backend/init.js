3aa70fef82194f237caea61cdf3dff75866368d3
Alex Tomovski <alext@wix.com>
2015-06-17 10:54:11 +0300

    Server side entry validation.

---
+++ backend/init.js
@@ -0,0 +1,12 @@
+import wixData from 'wix-data';
+
+wixData.registerBeforeInsertHook('entries', validateEntry);
+
+function validateEntry(entry) {
+
+	if (entry.name && entry.country && entry.email && entry.message && entry.message.length <= 600) {
+		return entry;
+	}
+
+	return Promise.reject(new Error('Failed to validate entry'));
+}
