ce519944a5c7972632ed00e724fed09ffd0f7d86
Ronen Burin <ronenb@wix.com>
2015-08-04 14:22:43 +0300

    Code format fixes

--- public/guestbook/bootstrap-show-errors-directive.js
+++ public/guestbook/bootstrap-show-errors-directive.js
@@ -1,34 +1,33 @@
 //This is almost the same as https://github.com/paulyoder/angular-bootstrap-show-errors TODO decide if we use the bower component for the tutorial.
-angular.module('guestbook').directive('showErrors', ['$timeout', function($timeout) {
+angular.module('guestbook').directive('showErrors', ['$timeout', function ($timeout) {
   return {
-	restrict: 'A',
-    require:  '^form',
+    restrict: 'A',
+    require: '^form',
     link: function (scope, el, attrs, formCtrl) {
-        var inputEl   = el[0].querySelector("[name]");
-        var inputNgEl = angular.element(inputEl);
-        var inputName = inputNgEl.attr('name');
-
-        scope.$watch(function() {
-          return formCtrl[inputName] && formCtrl[inputName].$invalid;
-        }, function(invalid) {
-    		  if(formCtrl[inputName].$touched){
-    		      toggleClasses(invalid);
-    		  }
-        });
-
-        scope.$on('show-errors-check-validity', function() {
-		  toggleClasses(formCtrl[inputName].$invalid);
-		});
-
-		scope.$on('show-errors-reset', function() {
-		  $timeout(function() {
-		    el.removeClass('has-error');
-		  }, 0, false);
-		});
-
-		function toggleClasses(invalid) {
-          el.toggleClass('has-error', invalid);
+      var inputEl = el[0].querySelector("[name]");
+      var inputNgEl = angular.element(inputEl);
+      var inputName = inputNgEl.attr('name');
+      scope.$watch(function () {
+        return formCtrl[inputName] && formCtrl[inputName].$invalid;
+      }, function (invalid) {
+        if (formCtrl[inputName].$touched) {
+          toggleClasses(invalid);
         }
+      });
+
+      scope.$on('show-errors-check-validity', function () {
+        toggleClasses(formCtrl[inputName].$invalid);
+      });
+      
+      scope.$on('show-errors-reset', function () {
+        $timeout(function () {
+          el.removeClass('has-error');
+        }, 0, false);
+      });
+
+      function toggleClasses(invalid) {
+        el.toggleClass('has-error', invalid);
+      }
     }
   };
 }]);
