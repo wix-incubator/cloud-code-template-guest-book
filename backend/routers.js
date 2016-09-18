05452f0dbd92b884cae59f95ad92f026e51e5e40
sang <sangolan2@gmail.com>
2016-09-18 16:42:46 +0300

    added routers.js

---
+++ backend/routers.js
@@ -0,0 +1,14 @@
+import {ok} from 'wix-router';
+let i = 0;
+
+export function date_router(request) {
+    return ok('page', new Date().toString());
+}
+
+export function counter_router(request) {
+    return ok('page', i++);
+}
+
+export function my_sitemap(sitemapRequest) {
+    return Promise.resolve([]);
+}
